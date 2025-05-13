25b020fe1715a3fdc3d966046091422f773a9b30
mahan <mahan.asadiiii@gmail.com>
2025-05-13 12:44:51 +0330

    Update dependencies in package.json and pnpm-lock.yaml; modify Home component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-
+      <main className="flex flex-col items-center sm:items-start">
+        Acsid Maad
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      {/* <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
 
-      </footer>
+      </footer> */}
     </div>
   );
 }
